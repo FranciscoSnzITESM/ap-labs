f03c4756ce20a4d1493221e0ffa2eb8d79915502
Francisco Sanchez Salomon <a01634745@itesm.mx>
2020-03-02 17:57:18 -0600

    String Manipulation Library Solved
    Signed-off-by: Francisco Sanchez Salomon <a01634745@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+	if(argc!=4){
+		printf("Only insert 3 arguments\n");
+		return -1;
+	}
+	printf("Initial Lenght: %d\n", mystrlen(argv[1]));
+	printf("New String: %s\n", mystradd(argv[1],argv[2]));
+	printf("Substring was found: %s\n", mystrfind(argv[2],argv[3]) ? "Yes" : "No");
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,34 @@
-
-
+#include <string.h>
+#include <stdlib.h>
 int mystrlen(char *str){
-    return 0;
+	int i;
+	for(i=0;str[i]!='\0';i++);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+	int s1 = mystrlen(origin);
+	int s2 = mystrlen(addition);
+	char *newstr = calloc(s1+s2+1, sizeof(char));
+	strcpy(newstr,origin);
+	strcpy(newstr+s1,addition);
+    return newstr;
 }
 
 int mystrfind(char *origin, char *substr){
+	int i,j, isFound;
+	int s1 = mystrlen(origin);
+	int s2 = mystrlen(substr);
+	for(i=0;i<s1;i++){
+		isFound = 1;
+		for(j=0;i+j<s1 && j<s2;j++){
+			if(origin[i+j] != substr[j]){
+				isFound = 0;
+				break;
+			}
+		}
+		if(isFound && j==s2)
+			return 1;
+	}
     return 0;
 }
