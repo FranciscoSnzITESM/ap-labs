688c53dfb5ee45607934e682f3becaa0bac24b7c
Francisco Sanchez Salomon <a01634745@itesm.mx>
2020-03-03 20:52:46 -0600

    Go-Slices-Maps Solved
    Signed-off-by: Francisco Sanchez Salomon <a01634745@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	arr := strings.Fields(s)
+	mapa := make(map[string]int)
+	for _,v := range arr {
+		mapa[v]+=1
+	}
+	return mapa
+}
+
 func main() {
+	wc.Test(WordCount)
 }
