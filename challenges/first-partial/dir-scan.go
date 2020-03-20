c10a28a15729b8b52e9ccd2c47f2b776e5840ab6
Francisco Sanchez Salomon <a01634745@itesm.mx>
2020-03-20 14:59:35 -0600

    Solved and Fixed first-partial
    Signed-off-by: Francisco Sanchez Salomon <a01634745@itesm.mx>

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -47,15 +47,15 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	str := 	   "+-------------------------+------+"
+	str := 	   "+-----------------+----------+"
 	fmt.Println("Directory Scanning Tool")
 	fmt.Println(str)
-	fmt.Printf("| Path |%s |\n", os.Args[1])
+	fmt.Printf("| Path            | %-9s|\n", os.Args[1])
 	fmt.Println(str)
-	fmt.Printf("| Directories |%d |\n", nDir)
-	fmt.Printf("| Symbolic Links |%d |\n", nSL)
-	fmt.Printf("| Devices |%d |\n", nDev)
-	fmt.Printf("| Sockets |%d |\n", nSock)
-	fmt.Printf("| Other files |%d |\n", nOthers)
+	fmt.Printf("| Directories     | %-9d|\n", nDir)
+	fmt.Printf("| Symbolic Links  | %-9d|\n", nSL)
+	fmt.Printf("| Devices         | %-9d|\n", nDev)
+	fmt.Printf("| Sockets         | %-9d|\n", nSock)
+	fmt.Printf("| Other files     | %-9d|\n", nOthers)
 	fmt.Println(str)
 }
