b31cbb4133bd62b380b9f464d477f366a6841084
Francisco Sanchez Salomon <a01634745@itesm.mx>
2020-05-15 01:12:13 -0500

    save progress

--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,40 @@
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include "logger.h"
+#include <ftw.h>
 
-int main(){
-    // Place your magic here
+static int treeEntry(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+    switch (typeflag)
+    {
+    case FTW_F: // Regular file
+        infof("Regular file: %s\n", fpath);
+        break;
+    case FTW_D: // Directory
+        infof("Directory: %s\n", fpath);
+        break;
+    case FTW_DNR: // Directory that can't be read
+        errorf("This program doesn't have permissions to read the directory %s\n", fpath);
+        return -1;
+    case FTW_NS: // Error calling stat(), not symbolic
+        warnf("Error calling stat, not a symbolic link\n");
+        break;
+    case FTW_SL:
+        infof("Symbolic link, not following it\n");
+        break;
+    default:
+        warnf("Unknown flag\n");
+        break;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    if (argc != 2){
+        errorf("Wrong parameters, usage: %s <directoryPath>\n", argv[0]);
+        return -1;
+    }
+    int flags = 0;
+    flags |= FTW_PHYS;
+    int err = nftw(argv[1], treeEntry, 20, flags);
     return 0;
 }
