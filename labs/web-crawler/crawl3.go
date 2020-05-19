5e631e43f511650ea5c84a153b6089a0ab1a1427
Francisco Sanchez Salomon <a01634745@itesm.mx>
2020-05-19 14:05:13 -0500

    solve web crawler

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -16,7 +16,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"strings"
+	"strconv"
 	"gopl.io/ch5/links"
 )
 
@@ -24,44 +25,89 @@ import (
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
+var depth = -1
+type Worklist struct {
+	links []string
+	currDepth int
+}
 
-func crawl(url string) []string {
+func crawl(url string, currDepth int) Worklist {
 	fmt.Println(url)
-	tokens <- struct{}{} // acquire a token
-	list, err := links.Extract(url)
-	<-tokens // release the token
+	// Freedom
+	if currDepth == -1 { 
+		tokens <- struct{}{} // acquire a token
+		list, err := links.Extract(url)
+		<-tokens // release the token
 
-	if err != nil {
-		log.Print(err)
+		if err != nil {
+			log.Print(err)
+		}
+		return Worklist{list, currDepth}
 	}
-	return list
+	// Depth Limit
+	if currDepth < depth{
+		tokens <- struct{}{} // acquire a token
+		list, err := links.Extract(url)
+		<-tokens // release the token
+
+		if err != nil {
+			log.Print(err)
+		}
+		return Worklist{list, currDepth + 1}
+	}
+	return Worklist{nil, depth}
 }
 
 //!-sema
 
 //!+
 func main() {
-	worklist := make(chan []string)
+	if 	len(os.Args)<2 || 
+	(strings.HasPrefix(os.Args[1], "-") && 
+	(len(os.Args) < 3 || !strings.HasPrefix(os.Args[1], "-depth=") || len(os.Args[1]) < 8)) {
+		fmt.Println("Wrong parameters, usage:\ngo run crawl3.go [-depth=<number>] <links>")
+		return
+	}
+	if strings.HasPrefix(os.Args[1], "-depth="){
+		// fmt.Println(os.Args[1][7:])
+		i, err := strconv.Atoi(os.Args[1][7:])
+		if err != nil {
+			fmt.Println("Cannot convert to integer the value specified in -depth=(value)")
+			return
+		}
+		if i <= 0 {
+			fmt.Println("Depth has to be higher than 0")
+			return
+		}
+		depth = i
+		fmt.Println("Defined depth:", depth)
+	}
+	worklist := make(chan Worklist)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
+	if depth == -1 {  // Freedom
+		go func() { worklist <- Worklist{os.Args[1:], -1} }()
+	} else { // Depth limit
+		go func() { worklist <- Worklist{os.Args[2:], 0} }()
+	}
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+		if list.links != nil {
+			for _, link := range list.links {
+				if !seen[link] {
+					seen[link] = true
+					n++
+					go func(link string) {
+						worklist <- crawl(link, list.currDepth)
+					}(link)
+				}
 			}
 		}
 	}
 }
-
 //!-
